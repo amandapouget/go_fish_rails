b4534790307b648e1f4da6ef5dbdaf56b809f9d3
Amanda Simon <mandysimon88@gmail.com>
2015-12-17 12:11:28 -0500

    Finish preliminary controllers refactor.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, class_name: 'RealUser', :controllers => {:registrations => "registrations"}
-  resources :users
 
   resources :matches, except: [:edit, :destroy]
   post '/start_with_robots' => 'matches#start_with_robots'
